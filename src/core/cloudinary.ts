aad7fd231a65b65785237fd652dc101a0f631f67
DarkMasterChe <50092737+DarkMasterChe@users.noreply.github.com>
2019-10-11 19:04:53 +0800

    Update cloudinary.ts

--- src/core/cloudinary.ts
+++ src/core/cloudinary.ts
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary');
 
 
 cloudinary.config({
-    cloud_name: "dolxtj5eo",
-    api_key: 856744385274757,
-    api_secret: "dZcR1du5ZPm-bK_LBF9SethyNWQ"
+    cloud_name: "",
+    api_key: ,
+    api_secret: ""
 });
 
 export default cloudinary;
